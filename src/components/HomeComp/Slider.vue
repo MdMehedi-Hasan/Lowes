657fe274f9ee10d5196ed952e26ccbc30ac10e6f
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-02 17:30:59 +0600

    slider confique done

--- src/components/HomeComp/Slider.vue
+++ src/components/HomeComp/Slider.vue
@@ -1,21 +1,37 @@
+<style>
+img{
+    max-width: 100%;
+    height: auto;
+    width: 100%;
+    max-height: 400px;
+}
+</style>
 <template>
-  <swiper
-    :spaceBetween="30"
-    :effect="'fade'"
-    :navigation="true"
-    :pagination="{
-      clickable: true,
+  <div class="container mx-auto">
+    <swiper
+      :spaceBetween="30"
+      :effect="'fade'"
+      :navigation="false"
+      :loop="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :autoplay="{
+      delay: 3500,
+      disableOnInteraction: false,
     }"
-    :modules="modules"
-    class="mySwiper">
-    <swiper-slide><img src="../../assets/banner.png" /></swiper-slide>
-    <swiper-slide><img src="../../assets/Network.png" /></swiper-slide>
-    <swiper-slide><img src="../../assets/fahim.png" /></swiper-slide>
-    <swiper-slide><img src="../../assets/1.png" /></swiper-slide>
-    <swiper-slide><img src="../../assets/2.png" /></swiper-slide>
-    <swiper-slide><img src="../../assets/3.png" /></swiper-slide>
-    <!-- <swiper-slide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></swiper-slide> -->
-  </swiper>
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide><img src="../../assets/banner.png" /></swiper-slide>
+      <swiper-slide><img src="../../assets/Network.png" /></swiper-slide>
+      <swiper-slide><img src="../../assets/fahim.png" /></swiper-slide>
+      <swiper-slide><img src="../../assets/1.png" /></swiper-slide>
+      <swiper-slide><img src="../../assets/2.png" /></swiper-slide>
+      <swiper-slide><img src="../../assets/3.png" /></swiper-slide>
+      <!-- <swiper-slide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></swiper-slide> -->
+    </swiper>
+  </div>
 </template>
 <script>
 // Import Swiper Vue.js components
@@ -28,9 +44,8 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
 // import required modules
-import { EffectFade, Navigation, Pagination } from "swiper";
+import { Autoplay,EffectFade, Navigation, Pagination } from "swiper";
 
 export default {
   components: {
@@ -39,7 +54,7 @@ export default {
   },
   setup() {
     return {
-      modules: [EffectFade, Navigation, Pagination],
+      modules: [Autoplay,EffectFade, Navigation, Pagination],
     };
   },
 };
