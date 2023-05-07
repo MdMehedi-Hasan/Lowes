03a182df21abb7d0b05f518c62f47e7bcd954ad7
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-07 15:15:16 +0600

    trying to intregate api

--- src/components/HomeComp/Trending.vue
+++ src/components/HomeComp/Trending.vue
@@ -1,148 +1,25 @@
+<script>
+import axios from 'axios'
+
+methods: {
+    getProducts()
+}
+let products
+let chiku = [1,2,3,4,5,6,7,8,9,10,11,12]
+function getProducts (){
+  fetch('http://localhost:3000/all-products')
+  .then(response=>response.json())
+  .then(data=>products=data)
+  .then(()=>console.log(products))
+}
+</script>
 <template>
   <h1 class="text-center font-bold text-2xl">Trending now</h1>
   <section class="container mx-auto grid grid-cols-6 gap-5">
-    <div class="card bg-base-100 shadow-xl">
+    <div v-for="product in chiku" :key="product._id" class="card bg-base-100 shadow-xl">
       <div class="card-body">
         <h2 class="card-title">Card title!</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
-      </div>
-    </div>
-    <div class="card bg-base-100 shadow-xl">
-      <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          
-        </div>
       </div>
     </div>
   </section>
