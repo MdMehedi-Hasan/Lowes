0fd4be0041e71c59d7db3774a849ab4313b35b30
MdMehedi-Hasan <mh19115@gmail.com>
2023-05-13 01:34:05 +0600

    fix-state issue solved, api integration in proccess

--- src/components/Common/ProductCard.vue
+++ src/components/Common/ProductCard.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="card card-compact bg-base-100 border rounded-md">
-  <figure><img src="../../assets/images-removebg-preview.png" alt="Shoes" /></figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes xyz!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+  <div class="card card-compact bg-base-100 border rounded-md">
+    <figure>
+      <img src="../../assets/images-removebg-preview.png" alt="Shoes" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes xyz!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+    </div>
   </div>
-</div>
 </template>
--- src/components/HomeComp/Trending.vue
+++ src/components/HomeComp/Trending.vue
@@ -4,28 +4,31 @@
 export default {
   data() {
     return {
-      // products: []
+      products: [],
     };
   },
   mounted() {
-    getProducts();
+    this.getProducts();
+  },
+  methods: {
+    async getProducts() {
+      await fetch("http://localhost:3000/all-products")
+        .then((response) => response.json())
+        .then((data) => (this.products = data));
+    },
   },
 };
-async function getProducts() {
-  /* fetch('http://localhost:3000/all-products')
-  .then(response=>response.json())
-  .then(data=>this.products = data) */
-  // .then(()=>console.log(products))
-}
 </script>
 <template>
   <h1 class="text-center font-bold text-2xl">Trending now</h1>
   <section class="container mx-auto grid grid-cols-6 gap-5">
-    <div class="card bg-base-100 shadow-xl">
-      <!-- v-for="product in products" :key="product._id" -->
+    <div v-for="product in products" :key="product._id" class="card card-compact bg-base-100 border rounded-md">
+      <figure>
+        <img :src="product?.picture" alt="Shoes" />
+      </figure>
       <div class="card-body">
-        <h2 class="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 class="card-title">{{product?.name}}</h2>
+        <p>{{ product?.description }}</p>
       </div>
     </div>
   </section>
